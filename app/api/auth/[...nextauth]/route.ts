ec4d65939f4ce15058b2d782297d800fd329be4e
jainex17 <jainexp017@gmail.com>
2024-09-17 16:24:02 +0530

    Refactor authentication configuration and database utility

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,47 +1,4 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
 import { handlers } from "@/auth"
-import prisma from "@/utils/prisma"
 
 export const { GET, POST } = handlers
-
-export const auth = NextAuth({
-  providers: [
-    Google({
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-  ],
-  callbacks: {
-    async signIn({ user }) {
-      
-      const email = user.email ?? "";
-      const name = user.name ?? "";
-      const picture = user.image ?? "";
-      if(email === "" || name === "" || picture === "") {
-        return false
-      }
-
-      const currentUser = await prisma.user.findUnique({
-        where: { email },
-      });
-
-      if (!currentUser) {
-        await prisma.user.create({
-          data: {
-            email,
-            name,
-            picture,
-          },
-        });
-      }
-
-      return true
-    },
-  },
-})
--- auth.ts
+++ auth.ts
@@ -1,6 +1,46 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { db } from "@/utils/db"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      
+      const email = user.email ?? "";
+      const name = user.name ?? "";
+      const picture = user.image ?? "";
+      
+      if(email === "" || name === "" || picture === "") {
+        console.log("Error: email, name, or picture is empty");
+        return false
+      }
+
+      const currentUser = await db.user.findUnique({
+        where: { email },
+      });
+
+      if (!currentUser) {
+        await db.user.create({
+          data: {
+            email,
+            name,
+            picture,
+          },
+        });
+      }
+
+      return true
+    },
+  },
 })
